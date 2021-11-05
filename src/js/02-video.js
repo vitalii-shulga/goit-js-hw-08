916b9263df5f1cc0440f15771d0a228b188f1b1d
Vitalii Shulga <v.shulga019@gmail.com>
2021-11-05 21:53:25 +0200

    Updated task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import throttle from 'lodash.throttle'
+import vimeoPlayer from '@vimeo/player'
 
+const CURRENT_TIME = 'videoplayer-current-time'
+const player = new vimeoPlayer(document.querySelector('#vimeo-player'))
+
+player.on('timeupdate', throttle(onPlay, 1000))
+
+if (localStorage.getItem(CURRENT_TIME)) {
+  player.setCurrentTime(Number.parseFloat(localStorage.getItem(CURRENT_TIME)))
+}
+
+function onPlay(event) {
+  localStorage.setItem(CURRENT_TIME, event.seconds === event.duration ? 0 : event.seconds)
+}
